840935221e16c7c3721730206b08e90eb5d2b8ad
romeoabulencia <romeo.abulencia@gmail.com>
2014-08-07 01:28:49 +0800

    Check deposit: Applied a more updated number of checks and total amount of checks.Added also 2 new fields total amount of checks and return checks.

--- check_deposit.py
+++ check_deposit.py
@@ -41,17 +41,71 @@ class glimsol_check_deposit(osv.osv):
         res = {}
         for obj in self.browse(cr, user, ids, context=context):
             res[obj.id]=sum([x.amount for x in obj.line_ids])
+            if context and 'line_ids' in context:
+                for x in context['line_ids']: print x
+                res[obj.id]=sum([x[2]['amount'] for x in context['line_ids']])
         return res        
+    
+    def _get_cleared_checks(self,cr,user,ids,name,attr,context=None):
+        res={}
+        
+        for obj in self.browse(cr, user, ids, context=context):
+            amount_list=[]
+            for x in obj.line_ids:
+                if x.state == 'paid':
+                    amount_list.append(x.amount)
+            res[obj.id]=sum(amount_list)
+            if context and 'line_ids' in context:
+                for x in context['line_ids']:
+                    if x[2]['state'] == 'paid':
+                        amount_list.append(x[2]['amount'])
+                res[obj.id]=sum(amount_list)            
+        return res
+    
+    
+    def _get_return_checks(self,cr,user,ids,name,attr,context=None):
+        res={}
+        
+        for obj in self.browse(cr, user, ids, context=context):
+            amount_list=[]
+            for x in obj.line_ids:
+                if x.state == 'returned':
+                    amount_list.append(x.amount)
+            res[obj.id]=sum(amount_list)
+            if context and 'line_ids' in context:
+                for x in context['line_ids']:
+                    if x[2]['state'] == 'returned':
+                        amount_list.append(x[2]['amount'])
+                res[obj.id]=sum(amount_list)                
+            
+        return res
+    
+    def onchange_line_ids(self,cr,uid,ids,line_ids,context=None):
+        print "onchange_line_ids".upper()
+        print "line_ids".upper(),line_ids
+        res={'value':{
+                      'cheque_number':len(line_ids),
+                      'cheque_amount':self._get_check_amount(cr, uid, ids, '','',{'line_ids':line_ids})[ids[0]],
+                      'cleared_checks':self._get_cleared_checks(cr, uid, ids, '','',{'line_ids':line_ids})[ids[0]],
+                      'return_checks':self._get_return_checks(cr, uid, ids, '','',{'line_ids':line_ids})[ids[0]]
+                      }}
+        #cheque_number _get_check_number
+        #cheque_amount _get_check_amount
+        #cleared_checks _get_cleared_checks
+        #return_checks _get_return_checks
+        print "res".upper(),res
+        return res
+    
     
     _columns={
               'partner_id':fields.many2one('res.partner', 'Customer', required=True),
               'invoice_id':fields.many2one('account.invoice', 'Invoice No.',required=True),
               'date': fields.date('Date'), 
               'line_ids':fields.one2many('glimsol.check.deposit.line', 'cd_id', 'Check Deposit', required=False),
-              'cheque_number': fields.function(_get_check_number, method=True, type='integer', string='Total number of checks', store=True), 
-              'cheque_amount':fields.function(_get_check_amount, method=True, type='float', string='Total amount of checks', store=False)
-              
-               
+              'cheque_number': fields.function(_get_check_number, method=True, type='integer', string='Total number of checks', store=False), 
+              'cheque_amount':fields.function(_get_check_amount, method=True, type='float', string='Total amount of checks', store=False),
+              'cleared_checks':fields.function(_get_cleared_checks,method=True,type='float', string="Total amount of checks cleared",store=False),
+              'return_checks':fields.function(_get_return_checks,method=True,type='float',string="Total amount of return checks",store=False),
               }
     _defaults={
         'date':time.strftime('%Y-%m-%d %H:%M:%S'),
